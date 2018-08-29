48dd040c186c82a2bcefc479063650b77d2942d0
andrewgarber <ngendesign@email.com>
2018-08-29 13:46:23 -0400

    Push added extension features

--- out/extension.js
+++ out/extension.js
@@ -6,40 +6,37 @@ var finJSONSchema = require('finxact-json');
 //Global prop
 var currentWindow = vscode.window;
 var currentTextEditor = currentWindow.activeTextEditor;
-//File utilities commands
+//File Untils
 const newFileFromTemplate = require('../utils/newfilefromtemplate');
-console.log(newFileFromTemplate);
 //var Ajv = require('ajv');
 //var ajv = new Ajv({allErrors: true});
-function FooBar(info) {
-    console.log(this);
-}
 function activate(context) {
-    // Check to see if is json file
     if (currentWindow && currentTextEditor.document.languageId === 'json') {
         //Condition not used
     }
     //Lets create a keybinding function
-    let disposable = vscode.commands.registerCommand('extension.finxactFormat', () => {
-        finFormat(currentTextEditor);
+    let format = vscode.commands.registerCommand('extension.finxactFormat', () => {
+        finFormat();
     });
+    // Don't really need this but I thought I would try some added fromating features
     let quickFormat = vscode.commands.registerCommand('extension.finxactQuickFormat', () => {
         //vscode.commands.executeCommand('editor.action.insertLineAfter')
-        //currentWindow.showInformationMessage("Just for testing");
-        finFormat(currentTextEditor);
+        finFormat();
     });
     let finValidate = vscode.commands.registerCommand('extension.finValidate', () => {
         //jsonValidate(currentTextEditor.document.getText());
-        vscode.commands.executeCommand("workbench.debug.action.toggleRepl");
+        //vscode.commands.executeCommand("workbench.debug.action.toggleRepl");
         console.info("Active From Foo");
         //currentWindow.showInformationMessage("Avtive from Foo");
     });
-    let foo = vscode.commands.registerCommand('extension.finxactJSONTemplate', newFileFromTemplate);
-    context.subscriptions.push(disposable, quickFormat, finValidate, foo);
+    let finxactNewFile = vscode.commands.registerCommand('extension.finxactJSONTemplate', newFileFromTemplate); // Create file from template
+    context.subscriptions.push(format, quickFormat, finValidate, finxactNewFile);
 }
 exports.activate = activate;
-//Combine everything into one thing Works for now
-let finFormat = function (editor) {
+// Combine everything into one func Works for now
+// THIS!!! All Night Long!!!! I had to create a new referance to the TextEditor
+function finFormat() {
+    let editor = vscode.window.activeTextEditor;
     var builder;
     const position = editor.selection.active;
     var newPosition = position.with(position.line, position.character);
@@ -47,25 +44,23 @@ let finFormat = function (editor) {
     var text = editor.document.getText(); //Format everything
     var processedText = finJSONSchema(text); //Bad coding all mashed together 
     builder = replaceAllContent(text, processedText);
-    /*Can be used to text selection formatting
-    if (editor.selection.isEmpty) {
-        var text = editor.document.getText();
-        var processedText = finJSONSchema(text);
-        builder = replaceAllContent(text, processedText);
-    }else{
-        editor.selection = newSelection;
-        var text = editor.document.getText(editor.selection);
-        var processedText = finJSONSchema(text);
-        builder = replaceSectionContent(text, processedText, editor.selection.start, editor.selection.end);
-        
-    }
-    */
+    /*Can be used to text selection formatting*/
+    //if (editor.selection.isEmpty) {
+    //    var text = editor.document.getText();
+    //    var processedText = finJSONSchema(text);
+    //    builder = replaceAllContent(text, processedText);
+    //}else{
+    //    editor.selection = newSelection;
+    //    var text = editor.document.getText(editor.selection);
+    //    var processedText = finJSONSchema(text);
+    //    builder = replaceSectionContent(text, processedText, editor.selection.start, editor.selection.end); 
+    //}
     //Replace editor with formatted text
     if (processedText != false) {
         vscode.window.activeTextEditor.edit(builder);
     }
     editor.selection = newSelection; //Reset the position after insert
-};
+}
 function jsonValidate(data) {
     //var validate = ajv.compile(require('./schema/v1.0.json'));
     //var data = {"foo": 1, "bar": 2}
@@ -80,7 +75,7 @@ function formatOption(currentWindow, currentTextEditor) {
             console.log(selection);
             if (selection == 'Yes') {
                 currentWindow.showInformationMessage('Finxact JSON Formater v2.0.0');
-                finFormat(currentTextEditor);
+                finFormat();
             }
         });
     }
